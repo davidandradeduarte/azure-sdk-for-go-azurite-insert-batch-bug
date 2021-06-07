314724441cf2a1b70e4a3c89374ba40944c6a3ba
David Duarte <davidandradeduarte@gmail.com>
2021-06-07 12:24:35 +0100

    adding gh issue link

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Reproducing a bug in Azure Go's SDK with Azurite when inserting in batch mode to a non-existing table
 
-[Github issue link](https://github.com/TODO)
+[Github issue link](https://github.com/Azure/azure-sdk-for-go/issues/14746)
+
+See [main.go](main.go):
+
+```golang
+func main(){
+    insert()
+    insertBatch()
+}
+
+func insert(){
+    // working, status code is TableNotFound as expected
+}
+
+func insertBatch(){
+    // not working, returns a EOF string error
+}
+```
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	tableCli storage.TableServiceClient
+	ts storage.TableServiceClient
 )
 
 const (
@@ -17,25 +17,23 @@ const (
 )
 
 func main() {
+	insertBatch("1", "3")
 	insert("1", "3")
-	query()
+	//query()
 }
 
-func insert(partitionkey string, rowkey string) {
-
+func insertBatch(partitionkey string, rowkey string) {
 	client, err := storage.NewEmulatorClient()
 
 	if err != nil {
 		fmt.Printf("%s: \n", err)
 	}
 
-	tableCli = client.GetTableService()
+	ts = client.GetTableService()
 
-	fmt.Println(tableCli)
+	t := ts.GetTableReference(tablename)
 
-	table := tableCli.GetTableReference(tablename)
-
-	entity := table.GetEntityReference(partitionkey, rowkey)
+	entity := t.GetEntityReference(partitionkey, rowkey)
 
 	props := map[string]interface{}{
 		"AmountDue":      200.23,
@@ -47,14 +45,14 @@ func insert(partitionkey string, rowkey string) {
 
 	entity.Properties = props
 
-	tb := table.NewBatch()
+	tb := t.NewBatch()
 
 	tb.InsertOrMergeEntity(entity, true)
 
 	if err := tb.ExecuteBatch(); err != nil {
 		if cerr, ok := err.(storage.AzureStorageServiceError); ok {
 			if cerr.Code == "TableNotFound" {
-				if cerr := table.Create(uint(10), storage.FullMetadata, nil); cerr != nil {
+				if cerr := t.Create(uint(10), storage.FullMetadata, nil); cerr != nil {
 					fmt.Printf("error creating table: %v.", cerr)
 					return
 				}
@@ -68,14 +66,55 @@ func insert(partitionkey string, rowkey string) {
 		}
 	}
 
-	// err = entity.Insert(fullmetadata, nil)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	} else {
 		fmt.Println("Inserted! ")
 		return
+	}
+}
+
+func insert(partitionkey string, rowkey string) {
+	client, err := storage.NewEmulatorClient()
+
+	if err != nil {
+		fmt.Printf("%s: \n", err)
+	}
+
+	ts = client.GetTableService()
+
+	t := ts.GetTableReference(tablename)
+
+	entity := t.GetEntityReference(partitionkey, rowkey)
+
+	props := map[string]interface{}{
+		"AmountDue":      200.23,
+		"CustomerCode":   "123",
+		"CustomerSince":  time.Date(1992, time.December, 20, 21, 55, 0, 0, time.UTC),
+		"IsActive":       true,
+		"NumberOfOrders": int64(255),
+	}
+
+	entity.Properties = props
+
+	err = entity.Insert(fullmetadata, nil)
+
+	if cerr, ok := err.(storage.AzureStorageServiceError); ok {
+		if cerr.Code == "TableNotFound" {
+			if cerr := t.Create(uint(10), storage.FullMetadata, nil); cerr != nil {
+				fmt.Printf("error creating table: %v.", cerr)
+				return
+			}
+			// retry
+			err = entity.Insert(fullmetadata, nil)
+		}
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Inserted!")
 	}
 }
 
@@ -87,11 +126,9 @@ func query() {
 		fmt.Printf("%s: \n", err)
 	}
 
-	tableCli = client.GetTableService()
+	ts = client.GetTableService()
 
-	fmt.Println(tableCli)
-
-	table := tableCli.GetTableReference(tablename)
+	table := ts.GetTableReference(tablename)
 
 	// timeout, metatadalevel, options
 	entities, err := table.QueryEntities(30, fullmetadata, nil)
