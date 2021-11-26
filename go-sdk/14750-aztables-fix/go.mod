9767b15c5c5095df8a82af439d9cbab2102fc656
David Duarte <davidandradeduarte@gmail.com>
2021-11-26 13:32:28 +0000

    aztables pruposed fix

---
+++ go-sdk/14750-aztables-fix/go.mod
@@ -0,0 +1,20 @@
+module playground
+
+go 1.17
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/data/aztables v0.4.0
+	github.com/Azure/go-autorest/autorest/to v0.4.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.20.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.8.1 // indirect
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210610132358-84b48f89b13b // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ go-sdk/14750-aztables-fix/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/data/aztables"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
+func main() {
+	insertBatch()
+	query()
+}
+
+func insertBatch() {
+	client, err := aztables.NewClientWithNoCredential("http://localhost:10002/TestTable", nil)
+	handle(err)
+	_, err = client.Create(context.Background(), nil)
+	handle(err)
+
+	entity := aztables.EDMEntity{
+		Entity: aztables.Entity{
+			PartitionKey: "pencils",
+			RowKey:       "id-003",
+		},
+		Properties: map[string]interface{}{
+			"Product":      "Ticonderoga Pencils",
+			"Price":        5.00,
+			"Count":        aztables.EDMInt64(12345678901234),
+			"ProductGUID":  aztables.EDMGUID("some-guid-value"),
+			"DateReceived": aztables.EDMDateTime(time.Now()),
+			"ProductCode":  aztables.EDMBinary([]byte("somebinaryvalue")),
+		},
+	}
+
+	data, err := json.Marshal(entity)
+	handle(err)
+
+	_, err = client.AddEntity(context.Background(), data, nil)
+	handle(err)
+}
+
+func query() {
+	client, err := aztables.NewClientWithNoCredential("http://0.0.0.0:10002", nil)
+	handle(err)
+	filter := "PartitionKey eq 'markers' or RowKey eq 'id-003'"
+	options := &aztables.ListEntitiesOptions{
+		Filter: &filter,
+		Select: to.StringPtr("RowKey,Value,Product,Available"),
+		Top:    to.Int32Ptr(15),
+	}
+
+	pager := client.List(options)
+	for pager.NextPage(context.Background()) {
+		resp := pager.PageResponse()
+		fmt.Printf("Received: %v entitiesn", len(resp.Entities))
+
+		for _, entity := range resp.Entities {
+			var myEntity aztables.EDMEntity
+			err = json.Unmarshal(entity, &myEntity)
+			handle(err)
+
+			fmt.Printf("Received: %v, %v, %v, %vn", myEntity.Properties["RowKey"], myEntity.Properties["Value"], myEntity.Properties["Product"], myEntity.Properties["Available"])
+		}
+	}
+
+	err = pager.Err()
+	handle(err)
+}
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
